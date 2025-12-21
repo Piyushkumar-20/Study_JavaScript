1ffb752febe03f6a2672d625ff55be1cb4867f63
Piyush Kumar <pk6686742@gmail.com>
2025-12-21 09:22:05 +0000

    file add

---
+++ loops.js/filters.js
@@ -0,0 +1,14 @@
+// const coding = ["Java", "ruby", "javascript", "Python", "CPP"]
+
+// const values = CSSGroupingRule.forEach( (item) => {
+//     //console.log(item)
+//     return item;
+// })
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter((num) => num > 5)
+// console.log(newNums);
+
