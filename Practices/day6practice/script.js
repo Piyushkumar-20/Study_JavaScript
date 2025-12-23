bafe9e5f387839bc468dafc4829ef65eb2caeda4
Piyush Kumar <pk6686742@gmail.com>
2025-12-23 12:25:27 +0000

    Day 6 Revision

--- Practices/day6practice/script.js
+++ Practices/day6practice/script.js
@@ -1,7 +1,26 @@
+//Problem 1
+const h1 = document.querySelector("#heading")
+console.log(h1)
 
+//Problem 2
+const h2 = document.querySelector("#title")
+console.log(h2)
 
-const firstlanguage = document.querySelector("title")
-firstlanguage.outerHTML = '<h1>TypeScript</h1>'
+//Problem 3
+const li = document.querySelectorAll("li")
+console.log(li)
+console.log(li[0].innerText)
 
-// Problem 7
+//Problem 4
+const parag = document.querySelector("p")
+console.log(parag)
+parag.style.color = "blue"
+parag.style.fontSize = "24px"
+
+//Problem 5
+let create = document.createElement("div")
+create.innerText = "DOM created"
+console.log(create)
+const news = document.querySelector("body").append(create)
+console.log(news)
 
