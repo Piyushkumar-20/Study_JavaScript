4ca9bdf4595412d89be6c61d77b22cce3de5d673
Piyush Kumar <pk6686742@gmail.com>
2025-12-28 08:39:04 +0000

    OOPS MID

--- OOPS/notes.md
+++ OOPS/notes.md
@@ -11,12 +11,17 @@
 ## Arts of OOP
     Object Literal (means exactly or litteraly)
 
-    - constructor function
+    - constructor function -> Always gives a new function
     - Prototypes
     - Classes
     - Instances(new, this)
+    - new keyword 
+- Step1 -> whenver use a (new) keyword an empty object is created which is called instances.
+- step2 -> Constructor function is called due to (new) keyword.
+- step3 -> all the arguments are injected into the (this) keyword.
+- step4 -> we get our functions.
 
-## 4 Pillars
+## Four Pillars
     - Abstraction -  detail Hide (Ex: Fetch)
     - Encapsulation - Many forms 
     - Inheritence
--- OOPS/oops.js
+++ OOPS/oops.js
@@ -4,10 +4,32 @@ const user = {
     isLoggedin: true,
 
     getuserdetails: function(){
-        console.log("Got user detail from database")
-
+        //console.log("Got user detail from database")
+        //console.log(`username: ${this.username}`)
+        //console.log(this)
     }
 }
 
-console.log(user.username)
-console.log(user.getuserdetails());
+// Constructor Function -> Always gives a new function
+
+//const promise1 = new Promise() // Here new keyword is a constructor function.
+//const date = new Date // new is used to create new instance.
+
+function User(username, logincount, isloggedin){
+    this.username = username;
+    this.logncount = logincount
+    this.isloggedin = isloggedin
+
+   // return this //implicitly return defined 
+}
+
+// new keyword 
+// Step1 -> whenver use a (new) keyword an empty object is created which is called instances.
+// step2 -> Constructor function is called due to (new) keyword.
+// step3 -> all the arguments are injected into the (this) keyword.
+// step4 -> we get our functions.
+
+const user1 = new User("Piyush", 12, true)
+const user2 = new User("Piyushi", 22, false)
+console.log(user1.constructor) 
+//console.log(user2)
