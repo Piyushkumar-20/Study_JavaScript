e94312ca2c25c8d9b8c9006c02be736a0b3ac618
Piyush Kumar <pk6686742@gmail.com>
2026-01-01 06:41:54 +0000

    objects

--- OOPS/mathpi.js
+++ OOPS/mathpi.js
@@ -1,19 +1,25 @@
 const value = Object.getOwnPropertyDescriptor(Math, "PI")
-console.log(value)
+//console.log(value)
 
 const chai = {
     name: "ginger chai",
     price: 250,
-    isAvailable: true
+    isAvailable: true,
+
+    orderchai: function(){
+        console.log("Chai nhi bani")
+    }
 }
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
 Object.defineProperty(chai, 'name', {
-    writable: false,
-    emuerable: false,
-    configurable: true
+    //writable: false,
+    emuerable: true,
+    //configurable: true
     })
 
 for (let [key,value] of Object.entries(chai)){
+    if (typeof value != 'function'){
         console.log(`${key}, ${value}`)
+    }
 }
